523c94bc6e5ab9843ba70a89537686e7629341bf
wtclosyn <losyn@lwork.com>
2015-04-25 22:44:39 +0800

    Hello Wold Spring Boot

--- SimpleSpringBoot/src/main/java/com/losyn/hello/controller/HelloController.java
+++ SimpleSpringBoot/src/main/java/com/losyn/hello/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * TODO: 这里要写注释的!
+ * Hello Wold Controller
  */
 @Controller
 @RequestMapping("/")
